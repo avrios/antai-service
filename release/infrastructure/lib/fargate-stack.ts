11be268b124b65b8f6d5cbd63404ec571adfba2b
Roger Jäggi <roger.jaeggi@avrios.com>
2021-03-01 22:59:56 +0100

    feat: set version in datadog
    Signed-off-by: Roger Jäggi <roger.jaeggi@avrios.com>

--- release/infrastructure/lib/fargate-stack.ts
+++ release/infrastructure/lib/fargate-stack.ts
@@ -154,10 +154,25 @@ export class FargateStack extends cdk.Stack {
                 'DD_ENV': this.stage.identifier,
                 'DD_SERVICE': this.stackNaming.serviceName,
                 'DD_SITE': 'datadoghq.eu',
+                'DD_VERSION': `${this.retrieveImageTag()}`,
                 'ECS_FARGATE': 'true'
             },
             logging: this.setupCloudWatchLogGroup('datadog-agent'),
             essential: true
+        });
+    }
+
+    private retrieveImageTag() {
+        return cdk.Lazy.string({
+            produce: () => {
+                // @ts-ignore
+                if (this.image.imageTagParameter) {
+                    // @ts-ignore
+                    return this.image.imageTagParameter.valueAsString;
+                } else {
+                    throw new Error('TagParameterContainerImage must be used in a container definition when retrieving tag parameter');
+                }
+            }
         });
     }
 
