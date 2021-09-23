c1f58e4b55b115b7f66b50625469064a64abd013
alastair-watts-avrios <54026832+alastair-watts-avrios@users.noreply.github.com>
2021-09-23 09:19:59 +0200

    feat: add security (#21)
    * feat: add security
    
    Jira-Ticket: AV-21548
    
    * feat: add example secured endpoints and secured config
    
    Jira-Ticket: AV-21548

---
+++ code/bp-api/src/main/java/com/avrios/blueprint/api/BlueprintApi.java
@@ -0,0 +1,24 @@
+package com.avrios.blueprint.api;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.avrios.girders.security.session.SecurityRoleName.COMPANY_ADMIN;
+import static com.avrios.girders.security.session.SecurityRoleName.READ_WRITE_USER;
+
+@RestController
+public class BlueprintApi {
+
+    @GetMapping
+    public String helloWorld(){
+        return "Hello world";
+    }
+
+    @PostMapping
+    @Secured({READ_WRITE_USER, COMPANY_ADMIN})
+    public String saveWorld(){
+        return "Saved the world";
+    }
+}
---
+++ code/bp-api/src/main/java/com/avrios/blueprint/config/WebSecurityConfig.java
@@ -0,0 +1,37 @@
+package com.avrios.blueprint.config;
+
+import com.avrios.girders.security.cors.AvrEnableCors;
+import com.avrios.girders.security.cors.DefaultCorsConfiguration;
+import com.avrios.girders.security.jwt.JwtConfiguration;
+import com.avrios.girders.security.jwt.JwtFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@Import({JwtConfiguration.class, DefaultCorsConfiguration.class})
+@EnableWebSecurity
+@AvrEnableCors
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true, jsr250Enabled = true, securedEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final JwtFilter jwtFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement().disable()
+                .cors().and()
+                .csrf().disable()
+
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                .regexMatchers("^/healthCheck.*").permitAll()
+                .anyRequest().authenticated();
+    }
+}
