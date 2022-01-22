1b2963f3d3d1c0fca3af216083f4f3595198c958
Roger Jäggi <roger.jaeggi@avrios.com>
2022-01-22 08:29:28 +0100

    chore: remove reference to log4j2
    Signed-off-by: Roger Jäggi <roger.jaeggi@avrios.com>

--- release/docker/src/main/docker/start-service.sh
+++ release/docker/src/main/docker/start-service.sh
@@ -5,5 +5,4 @@ JAR_FILE="$1"
 java    \
         -javaagent:/var/opt/tracking-agent/dd-java-agent.jar \
         -Dfile.encoding=UTF8 \
-        -Dlog4j.configurationFile=log4j2.xml \
         -jar "${JAR_FILE}" --server.port=${container.expose.port}
