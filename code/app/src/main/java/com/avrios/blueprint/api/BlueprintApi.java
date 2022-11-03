8c62c88635a3c44b0a5b40d75fa0135698fea57f
saif-romdhane <saifeddine.romdhane@avrios.com>
2022-11-03 12:50:47 +0100

    fix: Api not routable through the GW.
    - Add a base path for dummy controller

--- code/app/src/main/java/com/avrios/blueprint/api/BlueprintApi.java
+++ code/app/src/main/java/com/avrios/blueprint/api/BlueprintApi.java
@@ -3,12 +3,15 @@ package com.avrios.blueprint.api;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.avrios.girders.security.session.SecurityRoleName.COMPANY_ADMIN;
 import static com.avrios.girders.security.session.SecurityRoleName.READ_WRITE_USER;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
+@RequestMapping(path = "/dummy", produces = APPLICATION_JSON_VALUE)
 public class BlueprintApi {
     @GetMapping
     public String helloWorld() {
