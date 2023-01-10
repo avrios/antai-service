3345df630cfa5d1ba89424aa921e4ab18653cba1
Roger Jäggi <roger.jaeggi@avrios.com>
2023-01-10 09:29:35 +0100

    chore: dependency update 2023-01
    JIRA ticket: AV-25438
    
    Signed-off-by: Roger Jäggi <roger.jaeggi@avrios.com>

--- code/app/src/main/java/com/avrios/blueprint/config/WebSecurityConfiguration.java
+++ code/app/src/main/java/com/avrios/blueprint/config/WebSecurityConfiguration.java
@@ -2,8 +2,7 @@ package com.avrios.blueprint.config;
 
 import com.avrios.girders.security.cors.AvrEnableCors;
 import com.avrios.girders.security.cors.DefaultCorsConfiguration;
-import com.avrios.girders.security.jwt.JwtConfiguration;
-import com.avrios.girders.security.jwt.JwtFilter;
+import com.avrios.girders.security.jwt.AvrEnableJwtResourceServer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,26 +10,27 @@ import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static com.avrios.girders.security.jwt.AvrEnableJwtResourceServerConfigurer.avrConfigureJwtResourceServer;
 
 @Configuration
-@Import({JwtConfiguration.class, DefaultCorsConfiguration.class})
+@Import({DefaultCorsConfiguration.class})
 @EnableWebSecurity
 @AvrEnableCors
 @RequiredArgsConstructor
+@AvrEnableJwtResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true, jsr250Enabled = true, securedEnabled = true)
 public class WebSecurityConfiguration {
-    private final JwtFilter jwtFilter;
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .sessionManagement().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .apply(avrConfigureJwtResourceServer()).and()
                 .cors().and()
                 .csrf().disable()
 
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .regexMatchers("^/healthCheck.*").permitAll()
                 .anyRequest().authenticated();
