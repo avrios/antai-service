e15fec8c16475a2b5515776ad6d012cbdc8e7a2c
Adrian Grzywaczewski <adrian.grzywaczewski@gmail.com>
2024-07-19 17:18:36 +0200

    Feat/AV-28200 add swagger UI for local env (#120)
    * chore: Added swagger to blueprint
    
    * chore: Added properties to enable/disable swagger-ui
    
    * feat: Added Swagger UI for dev environment.
    
    * feat: Added Swagger UI config for dev environment.
    
    * feat: Added Swagger UI config for dev environment.

---
+++ code/app/src/main/java/com/avrios/blueprint/config/OpenApiConfiguration.java
@@ -0,0 +1,27 @@
+package com.avrios.blueprint.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfiguration {
+    public static final String X_AUTH_ID_TOKEN_HEADER = "X-Auth-Id-Token";
+
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI()
+                .components(
+                        new Components()
+                                .addSecuritySchemes(X_AUTH_ID_TOKEN_HEADER, new SecurityScheme()
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name(X_AUTH_ID_TOKEN_HEADER)))
+                .security(List.of(new SecurityRequirement().addList(X_AUTH_ID_TOKEN_HEADER)));
+    }
+}
--- code/app/src/main/java/com/avrios/blueprint/config/WebSecurityConfiguration.java
+++ code/app/src/main/java/com/avrios/blueprint/config/WebSecurityConfiguration.java
@@ -1,11 +1,14 @@
 package com.avrios.blueprint.config;
 
 import com.avrios.girders.security.jwt.AvrEnableJwtResourceServer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,18 +19,26 @@ import static com.avrios.girders.security.jwt.AvrEnableJwtResourceServerConfigur
 @AvrEnableJwtResourceServer
 @EnableMethodSecurity(jsr250Enabled = true, securedEnabled = true)
 public class WebSecurityConfiguration {
+    @Value("${springdoc.swagger-ui.enabled}")
+    private Boolean swaggerUiEnabled;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .securityContext(securityContext -> securityContext.requireExplicitSave(true))
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-            .apply(avrConfigureJwtResourceServer()).and()
-            .csrf().disable()
+            .sessionManagement(sessions -> sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .with(avrConfigureJwtResourceServer(), Customizer.withDefaults())
+            .csrf(AbstractHttpConfigurer::disable)
 
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/healthCheck/**").permitAll()
+                .requestMatchers(addSwaggerPermissions()).permitAll()
                 .anyRequest().authenticated());
 
         return http.build();
+    }
+
+    private String[] addSwaggerPermissions() {
+        return swaggerUiEnabled ? new String[]{"/swagger-ui/**", "/v3/api-docs/**"} : new String[0];
     }
 }
