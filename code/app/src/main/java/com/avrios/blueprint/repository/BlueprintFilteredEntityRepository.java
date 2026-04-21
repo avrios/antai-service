6e548196434b1a891b4981d80acd6253880f1fd0
Laur <laurentiu.ghita@shiftmove.com>
2026-04-21 17:06:01 +0200

    feat: scaffold infrastructure
    DARK-1106

--- code/app/src/main/java/com/avrios/blueprint/repository/BlueprintFilteredEntityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.avrios.blueprint.repository;
-
-import com.avrios.blueprint.model.BlueprintFilteredEntity;
-import com.avrios.girders.persistence.repository.filtered.FilteredCrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BlueprintFilteredEntityRepository extends FilteredCrudRepository<BlueprintFilteredEntity> {
-}
