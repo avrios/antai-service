d8a0680a941e3ff3062660da79250966216be8d2
Laur <laurentiu.ghita@shiftmove.com>
2026-04-23 14:44:35 +0200

    start app without db to allow first deployment

--- code/app/src/main/java/com/avrios/antai/AntaiServiceApplication.java
+++ code/app/src/main/java/com/avrios/antai/AntaiServiceApplication.java
@@ -2,10 +2,12 @@ package com.avrios.antai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @ConfigurationPropertiesScan
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class AntaiServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AntaiServiceApplication.class, args);
